60fdcd247120199abf8e726eab7a08013b4a875c
Jonathan Loos <jonathan.loos@web.de>
2019-08-31 21:59:09 +0200

    Add use latin option to Settings

--- app/src/main/java/com/nobodysapps/septimanapp/activity/SeptimanappActivity.kt
+++ app/src/main/java/com/nobodysapps/septimanapp/activity/SeptimanappActivity.kt
@@ -1,16 +1,33 @@
 package com.nobodysapps.septimanapp.activity
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.nobodysapps.septimanapp.application.SeptimanappApplication
+import com.nobodysapps.septimanapp.localization.LocaleHelper
+
 
 abstract class SeptimanappActivity: AppCompatActivity() {
     fun getSeptimanappApplication() : SeptimanappApplication = (application as SeptimanappApplication)
 
+    private var initialLocale: String? = null
     private var requestCode = 0
     private val requestPermissionLambdas: MutableMap<Pair<Int, String>, PermissionListener> = HashMap()
 
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        initialLocale = LocaleHelper.getPersistedLocale(this)
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(LocaleHelper.onAttach(base))
+    }
+
+    // for requesting Permissions
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -30,6 +47,7 @@ abstract class SeptimanappActivity: AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
+    // for requesting Permissions
     fun withPermission(permission: String, listener: PermissionListener) {
         if (ActivityCompat.checkSelfPermission(this, permission)
             != PackageManager.PERMISSION_GRANTED) {
@@ -41,6 +59,13 @@ abstract class SeptimanappActivity: AppCompatActivity() {
             requestCode++
         } else {
             listener.onPermissionGranted(permission)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (initialLocale != null && initialLocale != LocaleHelper.getPersistedLocale(this)) {
+            recreate()
         }
     }
 
--- app/src/main/java/com/nobodysapps/septimanapp/activity/SettingsActivity.kt
+++ app/src/main/java/com/nobodysapps/septimanapp/activity/SettingsActivity.kt
@@ -1,11 +1,18 @@
 package com.nobodysapps.septimanapp.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.nobodysapps.septimanapp.R
+import com.nobodysapps.septimanapp.localization.LocaleHelper
+import java.util.*
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
-class SettingsActivity : AppCompatActivity() {
+
+class SettingsActivity : SeptimanappActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +24,50 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    class SettingsFragment : PreferenceFragmentCompat(),
+        SharedPreferences.OnSharedPreferenceChangeListener {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+
+        override fun onSharedPreferenceChanged(
+            sharedPreferences: SharedPreferences?,
+            key: String?
+        ) {
+            if (context == null) return
+            when (key) {
+                KEY_USE_LATIN -> {
+                    val useLatin = PreferenceManager.getDefaultSharedPreferences(context)
+                        .getBoolean(key, false)
+                    PreferenceManager.getDefaultSharedPreferences(context).edit().putString(
+                        KEY_PREF_LANGUAGE, if (useLatin) "la" else "system"
+                    ).apply()
+                }
+                KEY_PREF_LANGUAGE -> {
+                    LocaleHelper.setLocale(
+                        context!!,
+                        PreferenceManager.getDefaultSharedPreferences(context).getString(key, "")
+                            ?: ""
+                    )
+                    activity?.recreate() // necessary here because this Activity is currently running and thus a recreate() in onResume() would be too late
+                }
+            }
+        }
+
+        override fun onResume() {
+            super.onResume()
+            // documentation requires that a reference to the listener is kept as long as it may be called, which is the case as it can only be called from this Fragment
+            preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        }
+
+        companion object {
+            const val KEY_USE_LATIN = "use_latin"
+            const val KEY_PREF_LANGUAGE = "language"
         }
     }
 }
